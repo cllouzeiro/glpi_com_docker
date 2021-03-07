7080210765d401435de8444f4e8401d2c1caee37
claudio <cl.louzeiro@gmail.com>
2021-03-07 09:07:54 -0300

    Primeiro Commit

---
+++ application/config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = 'glpi.mysql';
+   public $dbuser     = 'glpi_user';
+   public $dbpassword = '12345678';
+   public $dbdefault  = 'glpidb';
+}
